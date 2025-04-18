c5e8f71bdceb1b7b2513216d6f1e71135bb4f9de
Achaz91 <ngwouanza@gmail.com>
2025-04-18 13:48:16 +0100

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-
-import streamlit as st
-
-st.set_page_config(page_title="sio PWA Test", page_icon=":rocket:", layout="centered")
-
-st.title("Bienvenue sur sio PWA Test")
-st.write("Ceci est une petite application Streamlit pour tester le concept PWA.")
-st.success("Ajoutez-la à votre écran d'accueil depuis votre navigateur mobile !")
