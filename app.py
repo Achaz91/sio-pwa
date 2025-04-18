73a3acca0847c72ff882c40ee8674fed5e76e036
Achaz91 <ngwouanza@gmail.com>
2025-04-18 13:44:37 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+
+import streamlit as st
+
+st.set_page_config(page_title="sio PWA Test", page_icon=":rocket:", layout="centered")
+
+st.title("Bienvenue sur sio PWA Test")
+st.write("Ceci est une petite application Streamlit pour tester le concept PWA.")
+st.success("Ajoutez-la à votre écran d'accueil depuis votre navigateur mobile !")
